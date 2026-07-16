2bb78e1a4b7393211ae0850b83d2cc03797dfca3
sanny523388 <yangsanny523388@gmail.com>
2026-07-16 21:41:02 +0800

    update: 修改dev-App.java#2

--- src/App.java
+++ src/App.java
@@ -2,9 +2,14 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
+<<<<<<< HEAD
         System.out.println("main-update-#1-#2");
 
         System.out.println("dev-update-#1");
 
+=======
+        System.out.println("main-update-#1-#2-#3");
+        
+>>>>>>> 8f89c01 (update: 修改main-App.java#3)
     }
 }
